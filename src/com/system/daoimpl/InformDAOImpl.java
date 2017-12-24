75dcec5869e181515a6c98a5d2a4abdd0431512f
dengyicong <34739188+764525691@users.noreply.github.com>
2017-12-24 15:28:26 +0800

    frist test

--- src/com/system/daoimpl/InformDAOImpl.java
+++ src/com/system/daoimpl/InformDAOImpl.java
@@ -4,7 +4,11 @@ import com.system.dao.InformDAO;
 import com.system.model.Inform;
 import com.system.utils.JDBCUtil;
 
+import java.sql.Date;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Created by asus on 2017/12/24.
@@ -18,5 +22,19 @@ public class InformDAOImpl implements InformDAO {
         Object[]params = {inform.getInformid(),inform.getInformtime(),inform.getInforminfo()};
         int n = jdbcUtil.executeUpdate(sql,params);
         return n;
+    }
+
+    @Override
+    public List<Inform> getAll() throws SQLException {
+        String sql = " SELECT * FROM t_inform  ";
+        List<Object>list = jdbcUtil.excuteQuery(sql,null);
+        List<Inform> informs = new ArrayList<>();
+        for (Object object :list){
+            Map<String, Object> map = (Map<String, Object>) object;
+            Inform inform = new Inform((Integer)map.get("informid"),(Date)map.get("informtime"),map.get("informinfo").toString());
+            inform.setInformid((Integer)map.get("informid"));
+            informs.add(inform);
+        }
+        return informs;
     }
 }
---
+++ src/com/system/service/ApplyService.java
@@ -0,0 +1,7 @@
+package com.system.service;
+
+/**
+ * Created by asus on 2017/12/24.
+ */
+public class ApplyService {
+}
